7ef9d5c5fcac2c92385173a74a27f5c457ad1e3c
Rene <renegarcia1864@gmail.com>
2021-06-05 18:35:29 -0500

    Discord token

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const Discord = require('discord.js');
 
-const client = new Discord.client();
+const client = new Discord.Client();
 
-client.login('ODUwODY5MDYxOTgyMDkzMzQy.YLv_kA.dd3b_tEYxGcF3wSUkr3O6CzQzNM')
+client.once('ready', () => {
+    console.log('Eternals is online!');
+});
+
+
+
+
+
+
+
+
+
+client.login('ODUwODY5MDYxOTgyMDkzMzQy.YLv_kA.dd3b_tEYxGcF3wSUkr3O6CzQzNM'); 
+
+// discord bot is working but since token was posted on github, Token had to be regenerated 
+
